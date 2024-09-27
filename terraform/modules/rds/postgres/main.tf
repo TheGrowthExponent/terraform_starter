3250420961d606fdd236ceb55f2ab7beefb73160
Jason Pascoe <jason.pascoe@fortescue.com>
2024-09-27 16:01:04 +0800

    performance_insights_enabled

--- terraform/modules/rds/postgres/main.tf
+++ terraform/modules/rds/postgres/main.tf
@@ -5,17 +5,18 @@ resource "aws_db_subnet_group" "default" {
 }
 
 resource "aws_db_instance" "db_server" {
-  db_name                     = var.db_name
-  allocated_storage           = 20
-  instance_class              = "db.t3.small"
-  engine                      = "postgres"
-  username                    = var.db_admin_user
-  manage_master_user_password = true
-  db_subnet_group_name        = aws_db_subnet_group.default.name
-  license_model               = "postgresql-license"
-  port                        = 5432
-  multi_az                    = true
-  vpc_security_group_ids      = var.vpc_security_group_ids
-  skip_final_snapshot         = false
-  tags                        = var.tags
+  db_name                      = var.db_name
+  allocated_storage            = 20
+  instance_class               = "db.t3.small"
+  engine                       = "postgres"
+  username                     = var.db_admin_user
+  manage_master_user_password  = true
+  db_subnet_group_name         = aws_db_subnet_group.default.name
+  license_model                = "postgresql-license"
+  port                         = 5432
+  multi_az                     = true
+  vpc_security_group_ids       = var.vpc_security_group_ids
+  skip_final_snapshot          = false
+  performance_insights_enabled = true
+  tags                         = var.tags
 }
