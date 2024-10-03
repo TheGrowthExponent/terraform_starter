a4a341c6ec9955e124517d8cbe831c7fac8ba3d6
Jason Pascoe <jason.pascoe@fortescue.com>
2024-10-03 08:08:46 +0800

    Update outputs.tf

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -1,4 +1,4 @@
 output "db_dns_name" {
-  value       = aws_rds_cluster_endpoint.db_endpoint.endpoint
+  value       = aws_rds_cluster.db_cluster.endpoint
   description = "Fully qualified domain name of the postgresql database server"
 }
