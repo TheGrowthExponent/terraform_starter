164aa79b7ecc16259f1e530891db63af1af99da6
Jason Pascoe <jason.pascoe@fortescue.com>
2024-10-03 17:53:37 +0800

    fix route53

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -14,10 +14,4 @@ resource "aws_route53_record" "record" {
   zone_id = data.aws_route53_zone.selected.zone_id
   name    = "${var.host_name}.${data.aws_route53_zone.selected.name}"
   type    = "A"
-
-  alias {
-    name                   = var.load_balancer_dns_name
-    zone_id                = data.aws_route53_zone.selected.zone_id
-    evaluate_target_health = true
-  }
 }
