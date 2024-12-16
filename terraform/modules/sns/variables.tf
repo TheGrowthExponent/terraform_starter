994ce1926675eb3d4aaf0dac9b641eb5d3f76f9e
Jason Pascoe <jpascoe@growthexponent.com>
2024-12-16 11:38:26 +0800

    add missed files

--- terraform/modules/sns/variables.tf
+++ terraform/modules/sns/variables.tf
@@ -2,13 +2,17 @@
 variable "account_id" {
   description = "AWS Account ID"
   type        = string
-  default     = ""
+  sensitive   = true
+
+  validation {
+    condition     = length(var.account_id) == 12 && length(regexall("[^0-9]", var.account_id)) == 0
+    error_message = "The account number must be 12 characters, and only contain numbers."
+  }
 }
 
 variable "topic_name" {
   description = "The name of the SNS topic."
   type        = string
-  default     = ""
 }
 
 variable "notification_recipients" {
